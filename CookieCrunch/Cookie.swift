1de03dc9d2514569733cdb41a41a6ee7c2043020
Nnamdi Massally <nqm14a@lib-301-10-9117.local>
2019-04-25 13:28:22 -0500

    Added stuff to Cookie.swift

--- CookieCrunch/Cookie.swift
+++ CookieCrunch/Cookie.swift
@@ -26,4 +26,54 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import Foundation
+import SpriteKit
+
+// MARK: - CookieType
+enum CookieType: Int {
+  case unknown = 0, croissant, cupcake, danish, donut, macaroon, sugarCookie
+  
+  var spriteName: String {
+    let spriteNames = [
+      "Croissant",
+      "Cupcake",
+      "Danish",
+      "Donut",
+      "Macaroon",
+      "SugarCookie"]
+    
+    return spriteNames[rawValue - 1]
+  }
+  
+  var highlightedSpriteName: String {
+    return spriteName + "-Highlighted"
+  }
+
+}
+
+// MARK: - Cookie
+class Cookie: CustomStringConvertible, Hashable {
+  
+  var hashValue: Int {
+    return row * 10 + column
+  }
+  
+  static func ==(lhs: Cookie, rhs: Cookie) -> Bool {
+    return lhs.column == rhs.column && lhs.row == rhs.row
+    
+  }
+  
+  var description: String {
+    return "type:\(cookieType) square:(\(column),\(row))"
+  }
+  
+  var column: Int
+  var row: Int
+  let cookieType: CookieType
+  var sprite: SKSpriteNode?
+  
+  init(column: Int, row: Int, cookieType: CookieType) {
+    self.column = column
+    self.row = row
+    self.cookieType = cookieType
+  }
+}
